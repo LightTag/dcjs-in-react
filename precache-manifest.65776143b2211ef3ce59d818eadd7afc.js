1e7e42493ff69a5f7741bb1ab9df3e1b451dc382
tal <talolard@gmail.com>
2019-02-18 07:52:44 +0100

    Updates

---
+++ precache-manifest.65776143b2211ef3ce59d818eadd7afc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dcjs-in-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1601c3fd345a4294d62e",
+    "url": "/dcjs-in-react/static/js/runtime~main.1601c3fd.js"
+  },
+  {
+    "revision": "958facc055d81a15c4ec",
+    "url": "/dcjs-in-react/static/js/main.958facc0.chunk.js"
+  },
+  {
+    "revision": "ff33a029f6ced4e9fb0a",
+    "url": "/dcjs-in-react/static/js/2.ff33a029.chunk.js"
+  },
+  {
+    "revision": "958facc055d81a15c4ec",
+    "url": "/dcjs-in-react/static/css/main.99a05173.chunk.css"
+  },
+  {
+    "revision": "ff33a029f6ced4e9fb0a",
+    "url": "/dcjs-in-react/static/css/2.894c0ed0.chunk.css"
+  },
+  {
+    "revision": "ec6d1efdb4c1f2783ce0f4fac4486606",
+    "url": "/dcjs-in-react/index.html"
+  }
+];
